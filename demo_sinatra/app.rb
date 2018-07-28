687404cdc90cbf1091514e65512c87cdde976cbf
Prakriti-nith <prakriti.gupta10@gmail.com>
2018-07-29 02:11:53 +0530

    Sinatra handling events examples

--- demo_sinatra/app.rb
+++ demo_sinatra/app.rb
@@ -36,6 +36,11 @@ get '/highchartscss' do
   erb :highcharts_css, :layout => :highcharts_layout
 end
 
+get '/handlingevents' do
+  handling_events_googlecharts
+  erb :handling_events, :layout => :googlecharts_layout
+end
+
 def highchart_example
     # bar chart
     opts = {
@@ -693,4 +698,36 @@ def highcharts_css
 
   # initialize
   @column_graph = Daru::View::Plot.new(series_dt2, opts2)
+end
+
+def handling_events_googlecharts
+  Daru::View.plotting_library = :googlecharts
+
+  data = [
+          ['Year', 'Sales', 'Expenses'],
+          ['2013',  1000,      400],
+          ['2014',  1170,      460],
+          ['2015',  660,       1120],
+          ['2016',  1030,      540]
+  ]
+  user_options = {
+      listeners: {
+          page: "alert('The user is navigating to page ' + (e['page'] + 1));",
+          select: "alert('A table row was selected');"
+      }
+  }
+  @table = Daru::View::Table.new(data, {page: 'enable', pageSize: 2}, user_options)
+
+  user_options_chart = {
+      listeners: {
+          select: " var selection = chart.getSelection();
+                    var selectedValue = data_table.getFormattedValue(selection[0].row, 0);
+                    alert('You selected ' + selectedValue + ' year');",
+          # exports the chart to PDF format
+          ready: "var doc = new jsPDF();
+                  doc.addImage(chart.getImageURI(), 0, 0);
+                  doc.save('chart.pdf');"
+      }
+  }
+  @column_chart = Daru::View::Plot.new(@table.table, { type: :column, width: 800 }, user_options_chart)
 end
