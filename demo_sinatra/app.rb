dfddd1479a0fdc558b7383826fe3be85005a1c8a
Prakriti-nith <prakriti.gupta10@gmail.com>
2018-07-29 02:30:53 +0530

    Sinatra examples for importing data from google spreadsheet

--- demo_sinatra/app.rb
+++ demo_sinatra/app.rb
@@ -157,6 +157,28 @@ def googlecharts_example
     @table.table, type: :pie, is3D: true, adapter: :googlecharts, height: 500, width: 800)
   @geochart = Daru::View::Plot.new(
     @table.table, type: :geo, adapter: :googlecharts, height: 500, width: 800)
+
+  data_customers = 'https://docs.google.com/spreadsheets/d/1aXns2ch8y_rl9ZLxSYZIU5ewUB1ZNAg5O6iPLZLApZI/gviz/tq?header=1&tq='
+  query_customers = 'SELECT * WHERE A > 1'
+  data_customers << query_customers
+  @customers_table = Daru::View::Table.new(data_customers, adapter: :googlecharts)
+  @customers_chart = Daru::View::Plot.new(data_customers, {type: :line, adapter: :googlecharts})
+
+  query_products = 'SELECT A, H, O, Q, R, U LIMIT 5 OFFSET 8'
+  products = 'https://docs.google.com/spreadsheets/d/1XWJLkAwch5GXAt_7zOFDcg8Wm8' \
+          'Xv29_8PWuuW15qmAE/gviz/tq?gid=0&headers=1&tq='
+  products << query_products
+  column_chart_options = {
+    width: 600,
+    type: :column,
+    adapter: :googlecharts
+  }
+  table_options = {
+    adapter: :googlecharts,
+    showRowNumber: true
+  }
+  @products_table = Daru::View::Table.new(products, table_options)
+  @column_chart = Daru::View::Plot.new(products, column_chart_options)
 end
 
 def datatables_examples
