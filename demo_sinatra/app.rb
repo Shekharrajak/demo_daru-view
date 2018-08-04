552a51e5983ccaabf7140ec01739407f27725fe0
Prakriti-nith <prakriti.gupta10@gmail.com>
2018-08-04 17:21:17 +0530

    Added Charteditor Sinatra examples

--- demo_sinatra/app.rb
+++ demo_sinatra/app.rb
@@ -51,6 +51,11 @@ get '/chartwrapper' do
   erb :chart_wrapper, :layout => :googlecharts_layout
 end
 
+get '/charteditor' do
+  chart_editor
+  erb :chart_editor, :layout => :googlecharts_layout
+end
+
 get '/formatters' do
   formatters
   erb :formatters, :layout => :googlecharts_layout
@@ -936,6 +941,29 @@ def chart_wrapper
     type: :bar
   }
   @bar_basic_chart = Daru::View::Plot.new(@bar_basic_table.table, bar_basic_options, chart_class: 'Chartwrapper')
+end
+
+def chart_editor
+  # set the library, to plot charts
+  Daru::View.plotting_library = :googlecharts
+
+  data = [
+        ['Year', 'Sales', 'Expenses'],
+        ['2013',  1000,      400],
+        ['2014',  1170,      460],
+        ['2015',  660,       1120],
+        ['2016',  1030,      540]
+  ]
+  data_str = 'https://docs.google.com/spreadsheets/d/1aXns2ch8y_rl9ZLxSYZIU5ewUB1ZNAg5O6iPLZLApZI/gviz/tq?header=1&tq='
+  area_chart_options = {
+    type: :area
+  }
+
+  @area_table = Daru::View::Table.new(data, {}, chart_class: 'Charteditor')
+  @area_chart = Daru::View::Plot.new(@area_table.table, area_chart_options, chart_class: 'Charteditor')
+
+  @table = Daru::View::Table.new(data_str, {width: 500}, chart_class: 'Charteditor')
+  @plot = Daru::View::Plot.new(data, {width: 500}, chart_class: 'Charteditor')
 end
 
 def formatters
